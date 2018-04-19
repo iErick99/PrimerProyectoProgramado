cac128176b84d29e0b09fa17e2547fe6040505d0
Unknown <gonzacrc2@gmail.com>
2018-04-18 19:46:27 -0600

    Algoritmos update

--- Algoritmos.py
+++ Algoritmos.py
@@ -1,16 +1,25 @@
 
 class Algoritmos:
 
-    def quick_sort(self, A):
-	    quick_sort2(A, 0, len(A)-1)
-	
+    def ordenar(self, A, primero, ultimo):
+        for i in range (primero, ultimo):
+            indiceMenor = i
+            for j in range (i+1, ultimo+1):
+                if A[j] < A[indiceMenor]:
+                    indiceMenor = j
+            if indiceMenor != i:
+                A[i], A[indiceMenor] = A[indiceMenor], A[i]
+
     def quick_sort2(self, A, menor, mayor):
-        if (mayor-menor < maximo) and (menor < mayor): 
-            ordenar(A, menor, mayor)
+        if (mayor - menor < mayor) and (menor < mayor): 
+            self.ordenar(A, menor, mayor)
         elif menor < mayor:
-            p = particion(A, menor, mayor)
-            quick_sort2(A, menor, p - 1)
-            quick_sort2(A, p + 1, mayor)
+            p = self.particion(A, menor, mayor)
+            self.quick_sort2(A, menor, p - 1)
+            self.quick_sort2(A, p + 1, mayor)
+
+    def quick_sort(self, A):
+	    self.quick_sort2(A, 0, len(A)-1)
 
     def pivot_medio(self, A, menor, mayor):
 	    medio = (mayor + menor) // 2
@@ -18,7 +27,7 @@ class Algoritmos:
 	    return pivot
 	
     def particion(self, A, menor, mayor):
-	    indiceDelPivot = pivot_medio(A, menor, mayor)
+	    indiceDelPivot = self.pivot_medio(A, menor, mayor)
 	    valorDelPivot = A[indiceDelPivot]
 	    A[indiceDelPivot], A[menor] = A[menor], A[indiceDelPivot]
 	    borde = menor
@@ -30,28 +39,19 @@ class Algoritmos:
 	    A[menor], A[borde] = A[borde], A[menor]
 
 	    return borde
-	
-    def ordenar(self, A, primero, ultimo):
-        for i in range (primero, ultimo):
-            indiceMenor = i
-            for j in range (i+1, ultimo+1):
-                if A[j] < A[indiceMenor]:
-                    indiceMenor = j
-            if indiceMenor != i:
-                A[i], A[indiceMenor] = A[indiceMenor], A[i]
 			
     def sort5(self, A):
     
         if len(A) < 2: return A
-        pequeno = sort5([x for x in A[1:] if x <= A[0]]) 
-        grande = sort5([x for x in A[1:] if x > A[0]]) 
+        pequeno = self.sort5([x for x in A[1:] if x <= A[0]]) 
+        grande = self.sort5([x for x in A[1:] if x > A[0]]) 
         return sum([pequeno, [A[0]], grande], [])
 
     def median_of_medians(self, A, k):
     
-        sublists = [sort5(A[j:j+5]) for j in range(0, len(A), 5)]
+        sublists = [self.sort5(A[j:j+5]) for j in range(0, len(A), 5)]
         medians = [sublist[len(sublist) // 2] for sublist in sublists]
-        medians = sort5(medians)
+        medians = self.sort5(medians)
     
         pivot = medians[len(medians) // 2]
     
@@ -59,8 +59,7 @@ class Algoritmos:
         SR = [x for x in A if x > pivot]
     
         if len(SL) > k:
-            return median_of_medians(SL, k)
+            return self.median_of_medians(SL, k)
         elif len(SL) < k:
-            return median_of_medians(SR, k-len(SL)-1)
+            return self.median_of_medians(SR, k-len(SL)-1)
         return pivot
-
