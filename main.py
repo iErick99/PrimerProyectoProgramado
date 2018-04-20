b0a8e05092e6d5c6a4c9a2bd5a4f4106a033cab4
Unknown <gonzacrc2@gmail.com>
2018-04-20 00:41:19 -0600

    UPDATE FINAL

--- main.py
+++
@@ -1,6 +0,0 @@
-from median_of_median import median_of_medians
-
-A = [8, 33, 17, 51, 57, 49, 35, 11, 25, 37, 14, 3, 2, 13, 
-     52, 12, 6, 29, 32, 54, 5, 16, 22, 23, 7]
-	 
-print(median_of_medians(B, len(B) // 2))
